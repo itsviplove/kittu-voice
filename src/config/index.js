e06953a5678934ed5bcae34e3d174627d015f87f
Viplove bansal <itsviplove@gmail.com>
2026-05-06 22:57:48 +0530

    Update src/config/index.js

--- src/config/index.js
+++ src/config/index.js
@@ -8,6 +8,7 @@ export function loadConfig() {
     openClawApiKey: process.env.OPENCLAW_API_KEY || '',
     discordVoiceChannelId: process.env.DISCORD_VOICE_CHANNEL_ID || '1493512967776637092',
     discordVoiceAutoJoin: process.env.DISCORD_VOICE_AUTO_JOIN !== 'false',
+    discordVoiceAutoRespond: process.env.DISCORD_VOICE_AUTO_RESPOND === 'true',
     discordVoiceWelcomeText: process.env.DISCORD_VOICE_WELCOME_TEXT || 'Kittu Voice is online.',
     discordCommandPrefix: process.env.DISCORD_COMMAND_PREFIX || '!',
   };
